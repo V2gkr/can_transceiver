71f2ef82603adcbb1f3d69e8003b6a1d0df015a7
V2gkr <vova.grisenko.vg@gmail.com>
2026-05-25 20:51:27 +0200

    changed main to use class

--- main.cpp
+++ main.cpp
@@ -6,25 +6,19 @@
 #include <linux/can/raw.h>
 #include <unistd.h>
 #include <string.h>
-#define IFNAME "can0"
+#include "can_receiver.h"
+// #define IFNAME "can0"
 
-struct ifreq ifr;
-struct sockaddr_can can_addr;
+// struct ifreq ifr;
+// struct sockaddr_can can_addr;
 struct can_frame canframe;
 
 int main(void){
-    int sock_fd=socket(PF_CAN,SOCK_RAW,CAN_RAW);
-    memcpy(ifr.ifr_ifrn.ifrn_name,IFNAME,4);
-    ioctl(sock_fd,SIOCGIFINDEX,&ifr);
-
-    can_addr.can_family=AF_CAN;
-    can_addr.can_ifindex=ifr.ifr_ifru.ifru_ivalue;
-
-    bind(sock_fd,(const sockaddr *)&can_addr,sizeof(can_addr));
+    CanReceiver& can_receiver=CanReceiver::getInstance();
     int nbytes;
     while(1){
-        std::cout<<"\033[2J\033[H";
-        nbytes=read(sock_fd,&canframe,sizeof(can_frame));
+        //std::cout<<"\033[2J\033[H";
+        nbytes=can_receiver.GetCanMessage(&canframe,sizeof(struct can_frame));
         if(nbytes){
             nbytes=0;
             std::cout<<"message id:"<<canframe.can_id<<std::endl;
