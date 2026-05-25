43d8e71845f90c5d9f523ee687c792b9dbf1df43
V2gkr <vova.grisenko.vg@gmail.com>
2026-05-25 22:38:00 +0200

    fixed cout and added cmake

--- main.cpp
+++ main.cpp
@@ -13,6 +13,13 @@
 // struct sockaddr_can can_addr;
 struct can_frame canframe;
 
+union chartofloatconverter{
+    u_int8_t chars[4];
+    float floatval;
+};
+
+float CharToFloat(u_int8_t *arr);
+
 int main(void){
     CanReceiver& can_receiver=CanReceiver::getInstance();
     int nbytes;
@@ -21,13 +28,21 @@ int main(void){
         nbytes=can_receiver.GetCanMessage(&canframe,sizeof(struct can_frame));
         if(nbytes){
             nbytes=0;
-            std::cout<<"message id:"<<canframe.can_id<<"\n";
+            std::cout<<"\nmessage id: 0x"<<std::hex<<canframe.can_id<<"\n";
             std::cout<<"message data:";
-            for(u_int8_t i=0;i<canframe.len;i++)
-                std::cout<<(int)canframe.data[i]<<" ";
+            std::cout<<CharToFloat(canframe.data);
+            // for(u_int8_t i=0;i<canframe.len;i++)
+            //     std::cout<<(int)canframe.data[i]<<" ";
             std::cout<<std::endl;
         }
         // std::cout<<"\033[2J\033[H";
     }
     return 0;
+}
+
+float CharToFloat(u_int8_t arr[]){
+    chartofloatconverter conv;
+    for(u_int8_t i=0;i<4;i++)
+        conv.chars[i]=arr[i];
+    return conv.floatval;
 }
